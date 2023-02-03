8eaa99efa5957f8385ff411cf7f3da997c130377
Erik Edhagen <erik@digiswede.com>
2023-02-03 23:35:07 +0100

    Use env var for mongo connection

--- services/database.ts
+++ services/database.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
-const connectionString = "mongodb://127.0.0.1:27017/my_database";
+const connectionString =
+  process.env.MONGODB_CONNECTION || "mongodb://127.0.0.1:27017/my_database";
 
 export default {
   connect: () => {
